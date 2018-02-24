d70fc7cde1f960f29338065bf4d945b4f3c263d1
Niklan <niklanrus@gmail.com>
2018-02-24 14:39:58 +0500

    Updated Annotation properties and descriptions for them.

--- src/Annotation/YandexYml.php
+++ src/Annotation/YandexYml.php
@@ -5,19 +5,45 @@ namespace Drupal\yandex_yml\Annotation;
 use Drupal\Component\Annotation\Plugin;
 
 /**
+ * Annotation YandexYml.
+ *
+ * This annotation helps to map properties of elements to it's destination
+ * xml elements to make generation more easily and transparent.
+ *
  * @Annotation
  */
 class YandexYml extends Plugin {
 
   /**
+   * Type of value.
+   *
+   * This denotes where this value will be set.
+   *
+   * Allowed values:
+   *  - content: Value of property will be set as content of element.
+   *  - property: Value of property will be set as attribute value of element.
+   *
    * @var string
    */
   public $type;
 
   /**
+   * XML element name.
+   *
+   * Used for naming element for property.
+   *
    * @var string
-   * @required
    */
   public $elementName;
+
+  /**
+   * Property name.
+   *
+   * If type was set to "property" you must enter property name where it goes.
+   * This name will be attribute name and property will it's value.
+   *
+   * @var string
+   */
+  public $propertyName;
 
 }
